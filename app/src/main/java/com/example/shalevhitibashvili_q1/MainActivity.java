b888f57a3781ecc7592405ed49dea757137f019d
shalevhiti1 <tayger110807@gmail.com>
2024-01-20 22:20:22 +0200

    Q1

--- app/src/main/java/com/example/shalevhitibashvili_q1/MainActivity.java
+++ app/src/main/java/com/example/shalevhitibashvili_q1/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.shalevhitibashvili_q1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button) findViewById(R.id.button);
     }
+    public void updateText(View v){
+        button.setText("Oh, yea, I’ve been clicked!");
+   }
 }
